a56091133791880e006b1a42a1bf1427b97dcbe2
Johan Åsling <johan.asling@hotmail.com>
2017-11-18 16:51:10 +0100

    Added some more console logging

--- src/main/java/se/joas/euler/Main.java
+++ src/main/java/se/joas/euler/Main.java
@@ -18,18 +18,41 @@ public class Main {
         Instant end = Instant.now();
         System.out.println("Solution problem 1: " + answerProblem1);
         System.out.println("Time: " +  Duration.between(start, end));
-        
+        System.out.println("--------------------------------");
+
+        start = Instant.now();
         long answerProblem2 = Problem2.findSumOfEvenTermsFibonacci(4000000);
         System.out.println("Solution problem 2: " + answerProblem2);
-        
+        end = Instant.now();
+        System.out.println("Time: " + Duration.between(start, end));
+        System.out.println("--------------------------------");
+
+        start = Instant.now();
         long answerProblem3 = Problem3.getLargestPrimeFactor(600851475143L);
+        end = Instant.now();
         System.out.println("Solution problem 3: " + answerProblem3);
-        
+        System.out.println("Time: " + Duration.between(start, end));
+        System.out.println("--------------------------------");
+
+        start = Instant.now();
         long answerProblem4 = Problem4.getLargestPalindrome(999);
+        end = Instant.now();
         System.out.println("Solution problem 4: " + answerProblem4);
-        
+        System.out.println("Time: " + Duration.between(start, end));
+        System.out.println("--------------------------------");
+
+        start = Instant.now();
         long answerProblem5 = Problem5.getSmallestMultiple(20);
+        end = Instant.now();
         System.out.println("Solution problem 5: " + answerProblem5);
+        System.out.println("Time: " + Duration.between(start, end));
+        System.out.println("--------------------------------");
+
+        start = Instant.now();
+        long answerProblem6 = Problem6.getDifferenceBetweenSumOfSquaresAndSquareOfSum(100);
+        end = Instant.now();
+        System.out.println("Solution problem 6: " + answerProblem6);
+        System.out.println("Time: " + Duration.between(start, end));
     }
 
 }
